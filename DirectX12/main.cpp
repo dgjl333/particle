41853feee2cb83ff1d3586d73da2bd29c040099e
dgjl333 <tuhengjun@outlook.com>
2024-09-27 16:54:57 +0900

    init window

--- DirectX12/main.cpp
+++ DirectX12/main.cpp
@@ -1,6 +1,70 @@
 #include "Debug.h"
 #include <Windows.h>
+#include <tchar.h>
 
-void main()
+int windowHeight;
+int windowWidth;
+float windowSize = 0.5;
+
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam,LPARAM lparam)
 {
+	if (msg == WM_DESTROY)
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+int main()
+{
+	windowHeight = GetSystemMetrics(SM_CYSCREEN) * windowSize;
+	windowWidth = GetSystemMetrics(SM_CXSCREEN) * windowSize;
+
+	WNDCLASSEX w = {};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure;
+	w.lpszClassName = _T("DX12");
+	w.hInstance = GetModuleHandle(nullptr);
+	RegisterClassEx(&w);
+	RECT rect = { 0, 0, windowWidth, windowHeight };
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(
+		w.lpszClassName,
+		_T("Window"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		rect.right - rect.left,
+		rect.bottom - rect.top,
+		nullptr,
+		nullptr,
+		w.hInstance,
+		nullptr
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = {};
+	while (true)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+
+	UnregisterClass(w.lpszClassName, w.hInstance);
 }
