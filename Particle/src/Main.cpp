e0e0cc6e74140cda0c46900ccdb636aeb5387e51
dgjl333 <66619273+dgjl333@users.noreply.github.com>
2025-01-12 14:03:23 +0900

    added comment

--- Particle/src/Main.cpp
+++ Particle/src/Main.cpp
@@ -1,6 +1,6 @@
 #include "ParticleRenderer.h"
 
-int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) //app is not api aware
 {
 	ParticleRenderer renderer;
 }
